4a9d96d612d6746d5b91b408c90f413274b8a5e8
Bart Wietecki <bartekwieteckiyt@gmail.com>
2023-04-05 21:03:16 +0200

    Database connection properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-    }
+    public static void main(String[] args) throws IOException {
+
+        issApiConnection.connect();
+     }
+
+    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/";
+
+    private static final String DB_NAME_ENV = "DB_NAME";
+
+    private static final String DB_USER_ENV = "DB_USER";
+
+    private static final String DB_PASSWORD_ENV = "DB_PASSWORD";
+
+
 }
