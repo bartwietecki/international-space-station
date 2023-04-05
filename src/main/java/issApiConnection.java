9cd8364bc729f91638fd7bdfb729c138cdf1c204
rmuszynski7001 <rmuszynski7001@gmail.com>
2023-04-05 20:40:08 +0200

    added issApiConnection

--- src/main/java/issApiConnection.java
+++ src/main/java/issApiConnection.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class issApiConnection {
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class issApiConnection {
+
+    public static void connect() throws IOException {
+
+        URL url = new URL("http://api.open-notify.org/iss-now");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+
+        //Check if connect is made
+        int responseCode = conn.getResponseCode();
+
+        // 200 OK
+        if (responseCode != 200) {
+            throw new RuntimeException("HttpResponseCode: " + responseCode);
+        } else {
+
+            StringBuilder informationString = new StringBuilder();
+            Scanner scanner = new Scanner(url.openStream());
+
+            while (scanner.hasNext()) {
+                informationString.append(scanner.nextLine());
+            }
+            //Close the scanner
+            scanner.close();
+
+            System.out.println(informationString);
+        }
+    }
 }
