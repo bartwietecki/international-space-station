8c30f0caa5e264566a0d5f415709fca325c7bee7
Bart Wietecki <bartekwieteckiyt@gmail.com>
2023-04-06 20:47:30 +0200

    Adding entities, repositories, services and DTO's

--- src/main/java/org/DbConnection.java
+++ src/main/java/org/DbConnection.java
@@ -1,3 +1,5 @@
+package org;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
---
+++ src/main/java/org/repository/AstronautsRepository.java
@@ -0,0 +1,33 @@
+package org.repository;
+
+import jakarta.persistence.EntityManager;
+import org.DbConnection;
+import org.entity.Astronauts;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class AstronautsRepository {
+    public void insert(Astronauts astronauts) {
+        EntityManager entityManager = DbConnection.getEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(astronauts);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    public Set<Astronauts> findAll() {
+        EntityManager entityManager = DbConnection.getEntityManager();
+        List<Astronauts> astronauts = entityManager.createQuery("select a from Astronauts a", Astronauts.class).getResultList();
+        entityManager.close();
+        return new HashSet<>(astronauts);
+    }
+
+    public Astronauts findById(long selectedAstronautsId) {
+        EntityManager entityManager = DbConnection.getEntityManager();
+        Astronauts astronauts = entityManager.find(Astronauts.class, selectedAstronautsId);
+        entityManager.close();
+        return astronauts;
+    }
+}
